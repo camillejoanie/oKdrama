7114a100cb744a1241a1fe1bb26401fc8800b809
camille <huangcamillej@gmail.com>
2023-11-01 15:48:35 -0700

    added to versions

--- migrations/versions/20231101_152559_.py
+++ migrations/versions/20231101_152559_.py
@@ -7,7 +7,9 @@ Create Date: 2023-11-01 15:25:59.552827
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '417ad8a7250b'
